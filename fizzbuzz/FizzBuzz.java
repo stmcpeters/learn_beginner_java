8b247beb5ec280e0d6008baa22daa97a70205d2d
stmcpeters <stmcpeters@gmail.com>
2025-06-01 20:13:20 -0700

    fixes typos in comments and recompiles project files

--- fizzbuzz/FizzBuzz.java
+++ fizzbuzz/FizzBuzz.java
@@ -7,9 +7,10 @@ public class FizzBuzz {
 //            if number is divisible by 3 == "fizz"
             } else if (i % 3 == 0){
                 System.out.println("Fizz");
-//            if number is divisible by 3 == "fizz"
+//            if number is divisible by 5 == "buzz"
             } else if (i % 5 == 0){
                 System.out.println("Buzz");
+//                if number is not divisible by 3 or 5, print number
             } else {
                 System.out.println(i);
             }
