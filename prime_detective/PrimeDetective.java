3714efced82e73dbbc152ac3e574aba0faff2287
stmcpeters <stmcpeters@gmail.com>
2025-06-01 21:34:57 -0700

    add PrimeDirective class with main method and initial setup

---
+++ prime_detective/PrimeDetective.java
@@ -0,0 +1,15 @@
+// Import statement:
+
+class PrimeDirective {
+
+    // Add your methods here:
+
+
+    public static void main(String[] args) {
+
+        PrimeDirective pd = new PrimeDirective();
+        int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
+
+    }
+
+}
